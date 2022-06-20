d09a2fb15d1dd1bde9b7e68792e5734aac4e3438
Artem Chubenko <artem.chubenko@gmail.com>
2022-06-20 23:25:19 +0300

    Added a functionality

--- src/index.js
+++ src/index.js
@@ -2,37 +2,44 @@ import './css/styles.css';
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
+const oneCountry = document.querySelector('.country-info');
+
 console.log(input);
 let inputData = '';
 let answerList = '';
 
-// function markUp({ capital }) {
-
-//     `<li>${capital}</li>`;
-
-// }
-
 function getCountries() {
   fetch(
-    `https://restcountries.com/v2/name/${inputData}?fields=name,capital,currencies`
+    `https://restcountries.com/v2/name/${inputData}?fields=name,capital,flag,languages,population`
   )
     .then(responce => {
       return responce.json();
     })
     .then(countries => {
       answerList = '';
+
       if (countries.length > 10) {
-        return console.log('Too many countries');
-      } else {
+        list.innerHTML = '';
+        oneCountry.innerHTML = '';
+        return console.log(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (countries.length > 1) {
+        list.innerHTML = '';
+        oneCountry.innerHTML = '';
         countries.map(country => {
-          answerList += `<li>${country.name}</li>`;
+          answerList += `<li class="item"><img src="${country.flag}" alt="Flag of ${country.name}" class="flag"><p class="country_name">${country.name}</p></li>`;
           console.log(answerList);
-          return answerList;
+          list.innerHTML = answerList;
         });
+      } else if (countries.length === 1) {
+        const { name, flag, capital, population } = countries[0];
+        list.innerHTML = '';
+        console.log(countries);
+        answerList = `<ul><li><img src="${flag}" alt="Flag of ${name}" class="one-flag"><span class="one-name"> ${name}</span></li><li><p class="one-info"><b>Capital: </b>${capital}</p></li><li><p class="one-info"><b>Population: </b>${population}</p></li><li><p class="one-info"><b>Languages:</b> ${countries[0].languages[0].name}</p></li></ul>`;
+        // console.log(answerList);
+        oneCountry.innerHTML = answerList;
       }
-    })
-    .then(answerList => {
-      list.insertAdjacentHTML('afterbegin', answerList);
     });
 }
 
